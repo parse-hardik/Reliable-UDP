82c78973315d98ec09dc0d3124722021af1f0c37
parse-hardik <f20180062@hyderabad.bits-pilani.ac.in>
2021-03-18 13:53:20 +0530

    Adding protocol and server-client files

---
+++ Protocol.py
@@ -0,0 +1,12 @@
+import socket
+import hashlib
+
+class Protocol():
+    def __init__(self):
+        print("Reliable UDP Protocol Initiated")
+
+    def create_socket(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return sock
+
+Protocol()
