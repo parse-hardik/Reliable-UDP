97153f3b9d329ca84a4a2824270f76bd75fa8bd8
Hardik Parnami <f20180062@hyderabad.bits-pilani.ac.in>
2021-03-18 02:07:19 -0700

    Making data packet

--- Protocol.py
+++ Protocol.py
@@ -2,6 +2,9 @@ import socket
 import hashlib
 
 class Protocol():
+    separator = "**!**"
+    seq = 0
+    seqlimit = 1
     def __init__(self):
         print("Reliable UDP Protocol Initiated")
 
@@ -9,7 +12,12 @@ class Protocol():
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         return sock
 
-    def sendDataPacket(self, msg):
-        msg = msg.encode()
+    def handhake(self):
+        
+
+    def createDataPacket(self, msg, ack, syn, fin):
+        checksum = hashlib.sha1(msg.encode()).hexdigest()
+        data = str(self.seq) + self.separator + str(len(msg)) + self.separator + str(msg) + self.separator + str(checksum) + self.separator + str(syn) + self.separator + str(ack) + self.separator + str(fin)
+        return data
 
 # Protocol()
