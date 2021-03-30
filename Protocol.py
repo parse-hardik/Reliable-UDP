e61d6a9f7c3ad75166e8a065c613a237277e9e81
parse-hardik <f20180062@hyderabad.bits-pilani.ac.in>
2021-03-30 15:59:26 +0530

    sendData

--- Protocol.py
+++ Protocol.py
@@ -1,10 +1,15 @@
 import socket
 import hashlib
+from threading import Thread
 
 class Protocol():
     delim = "<!>"
+    msg_size = 200
+    window_size=5
     def __init__(self):
         print("Reliable UDP Protocol Initiated")
+        self.lockServer = threading.Lock()
+        self.LockClient = threading.Lock()
 
     def create_socket(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -26,10 +31,39 @@ class Protocol():
         data+=str(seq)
         return data
 
-    def sendDataPacket(self, msg):
+    def recvACK(self, ACK, TripleDUP):
+        ''' 
+        If lock on both: As we receive ack, put it in the ACK list, and increment corresponding next expected seq number in
+        TripleDUP list 
+        '''
+        return
+
+    def ThreadSend(self):
+        return
+
+    def sendDataPackets(self, msg):
+        ''' 
+        Array initalize for maintaing thread-packet mapping, size is  twice wndow_size
+        ACK array-same size 0-not received, 1- received, 2 - retrans
+        Triple-DUP ack array, intialized to 0, increment 1 when a corresponding next expected seq number received
+        '''
+        window_start = 0
+        window_end = self.window_size-1
+        arrThread = []
+        AckArray = []
+        TripleDUP = []
+        for i in 2*window_size:
+            arrThread[i] = Thread(target=self.ThreadSend, args=(AckArray, TripleDUP), name=i)
         msg = msg.encode()
 
 proto = Protocol()
 data = proto.makeDataPacket("hello", 0, 0, 0, 6)
 ack = proto.makeACKPacket(6, 3)
 print(data +"\n" + ack)
+
+# def func(var):
+#     var['four']=5
+
+# var = {'four':4}
+# func(var)
+# print('The variable is ',var)
