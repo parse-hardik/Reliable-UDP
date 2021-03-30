be4d6e60e6d3b37f5e154f1a9dc166fb920881a0
parse-hardik <f20180062@hyderabad.bits-pilani.ac.in>
2021-03-30 10:37:57 +0530

    Data Packet made

--- Protocol.py
+++ Protocol.py
@@ -2,6 +2,7 @@ import socket
 import hashlib
 
 class Protocol():
+    delim = "<!>"
     def __init__(self):
         print("Reliable UDP Protocol Initiated")
 
@@ -9,7 +10,19 @@ class Protocol():
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         return sock
 
+    def makeDataPacket(self, info, SYN, ACK, FIN, seq):
+        data=""
+        data+=str(SYN) + self.delim
+        data+=str(ACK) + self.delim
+        data+=str(FIN) + self.delim
+        data+=str(seq) + self.delim
+        data+=str(info.encode('ascii')) + self.delim
+        data+=str(hashlib.sha1(info.encode()).hexdigest()) + self.delim
+        return data
+
     def sendDataPacket(self, msg):
         msg = msg.encode()
 
-# Protocol()
+proto = Protocol()
+data = proto.makeDataPacket("hello", 0, 0, 0, 6)
+print(data)
