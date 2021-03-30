f71cb7fdb0453ace2eef0df93937191b256a5b64
parse-hardik <f20180062@hyderabad.bits-pilani.ac.in>
2021-03-31 01:04:05 +0530

    Updating server

--- server.py
+++ server.py
@@ -1,2 +1,16 @@
 from Protocol import Protocol
-import argparse, select
+import argparse
+
+def server(address):
+    proto = Protocol()
+    sock = proto.create_socket()
+    sock.bind(address)
+    sock.listen(64)
+    return
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Send and recieve Reliable UDP locally')
+    parser.add_argument('host')
+    parser.add_argument('-p', metavar='PORT', type=int, default=6000, help='Reliable UDP port')
+    args = parser.parse_args()
+    server((args.host, args.p))
